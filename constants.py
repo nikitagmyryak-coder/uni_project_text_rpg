3b2ce1d4b7784a58d27d85aebc656dfb80f161fa
Gmyryak Nikita <nikitagmyryak@gmail.com>
2026-05-11 21:20:54 +0200

    made constants.py with tuples of different names for different types of enemies/NPCs (used ai because lazy)

---
+++ constants.py
@@ -0,0 +1,28 @@
+OGR_NAMES = ("Ozar", "Ozag", "Burag", "Druzur", "Xuzor", "Vegigark", "Blaukig", "Blanikork", "Kusazug", "Yuwukork")
+
+HUMAN_NAMES = ("Brick", "Rick", "Ivan", "Dmitriy", "John", "Joe", "Skinny", "Tall", "Doc", "Hammer", "Jacket", "Steve",
+               "Bill", "Bone", "Jack", "Viktor", "Stan", "Mick", "Frank", "Anton", "Shorty", "Slim", "Tiny", "Biggs",
+               "Red", "Dutch", "Sawyer", "Axel", "Clutch", "Patch", "Spike", "Miller", "Flint", "Steel", "Butch", "Duke",
+               "Knuckles", "Cage")
+
+CULT_NAMES = ("Malphas", "Xarros", "Vane", "Kael", "Morthos", "Mordekai", "Scythe", "Zealot", "Acolyte",
+              "Brother Night", "Sister Void", "Father Doom", "Malakor", "Dread", "Zalros", "Nyx", "Umbra", "Cipher",
+              "Bane", "Arkon", "Belial", "Vex", "Obsidian", "Sorrow", "Hollow", "Vesper", "Sanguine", "Prophet",
+              "Disciple", "Omen", "Relic", "Curse", "Blight", "Abyss", "Grim", "Sigil", "Rune", "Ether",
+              "Solace", "Gloom")
+
+
+MERC_NAMES = ("Viper", "Ghost", "Reaper", "Bishop", "Zero", "Echo", "Sarge", "Gunner", "Kilo", "Sabre", "Hunter",
+              "Falcon", "Wolf", "Ajax", "Nomad", "Titan", "Raptor", "Stryker", "Odin", "Goliath", "Bane", "Ronin",
+              "Hedge", "Sledge", "Talon", "Breaker", "Wardog", "Maverick", "Colt", "Steel", "Jester", "Vanguard",
+              "Rogue", "Outlaw", "Flint", "Trace", "Vortex", "Slayer", "Major", "Shield")
+
+GHOUL_NAMES = ("Rot", "Gnasher", "Carrion", "Skitter", "Pale", "Fester", "Marrow", "Shambler", "Grip", "Lurker", "Bile",
+               "Gristle", "Sliver", "Crawler", "Rags", "Jaw", "Gut", "Wretch", "Blight", "Corpse", "Snapper", "Grimace",
+               "Sludge", "Hollow", "Twitch", "Gibber", "Flay", "Retch", "Mangler", "Shred", "Vile", "Scrapper", "Howler",
+               "Clicker", "Gore", "Dreg", "Waste", "Pox", "Cadaver", "Slink")
+
+SOLDIER_NAMES = ("Kovacs", "Miller", "Schmidt", "Petrov", "Volkov", "Chen", "Sato", "Wagner", "Rossi", "Dubois",
+                 "Hassan", "Diaz", "Silva", "Andersson", "Novak", "Larsen", "Kim", "Lee", "Park", "Grant", "Fisher",
+                 "Stone", "Cross", "Black", "Steele", "Mason", "Wolf", "Hunter", "Becker", "Horton", "Dixon", "Price",
+                 "Vance", "Reid", "Hayes", "Foster", "Brooks", "Banks", "Kelly", "Ward")
--- entities/player.py
+++ entities/player.py
@@ -1,6 +1,6 @@
 from entities.entities import Entity
 
 class Player(Entity):
-    def __init__(self, name, hp, damage, level=1, race="Human"):
+    def __init__(self, name, hp, damage,  level=1, race="Human"):
         super().__init__(name, race, level, hp, damage)
         self.inventory = []
