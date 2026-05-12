39be0e22152d08efcdb32b37d0b26c10ec11ae7f
Gmyryak Nikita <nikitagmyryak@gmail.com>
2026-05-12 12:54:24 +0200

    added exceptions_and_functions file and moved todo list there

--- entities/player.py
+++ entities/player.py
@@ -1,5 +1,5 @@
 from entities.entities import Entity
-from exceptions.exceptions import *
+from exceptions_and_functions.exceptions import *
 import random
 import re
 
@@ -30,15 +30,3 @@ class Player(Entity):
     #         self._level = value
     #         print(f"Level Up! Now you are level {value}")
 
-# todo:
-# [] health_restore()
-# [] heal()
-# [] equip()
-# [] inspect()
-# [] gold/money
-# [] drop_item()
-# [] capacity based on level
-# [] base_damage vs total_damage
-# [] take_damage
-# [] weapon_damage
-# [] cheat
---
+++ exceptions_and_functions/different_finctions.py
@@ -0,0 +1,12 @@
+# todo:
+# [] health_restore()
+# [] heal()
+# [] equip()
+# [] inspect()
+# [] gold/money
+# [] drop_item()
+# [] capacity based on level
+# [] base_damage vs total_damage
+# [] take_damage
+# [] weapon_damage
+# [] cheat
