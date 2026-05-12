54f3c6308c5f4b6e261eac1dbdcad7bdff296f6f
Gmyryak Nikita <nikitagmyryak@gmail.com>
2026-05-12 12:46:45 +0200

    fixed Cultist damage calculator

--- entities/alive.py
+++ entities/alive.py
@@ -3,7 +3,7 @@ from entities.entities import Entity
 class Cultist(Entity):
     def __init__(self, level):
         hp = level * 25
-        damage = hp * 25
+        damage = level * 25
         super().__init__(None, "cultist", level, hp, damage)
 
 
