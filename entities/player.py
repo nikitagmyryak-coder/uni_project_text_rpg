1bc1005c1f8c6023e7ec22852aff40451b48fd48
Gmyryak Nikita <nikitagmyryak@gmail.com>
2026-05-12 12:21:17 +0200

    started doing valid/invalid name systtem, still need to add custom exceptions

--- entities/player.py
+++ entities/player.py
@@ -1,12 +1,16 @@
 from entities.entities import Entity
 import random
+import re
 
 class Player(Entity):
     # we insert level in the beginning of the game so i can add a cheat later to test staff
     def __init__(self, name, level=1):
         max_hp = 100 + (level * 20)
         base_damage = 15 + (level * 10) # + weapon_damage
-
+        if re.match("^[A-Za-z]+$", name):
+            self.name = name
+        # else:
+        #     raise InvalidNameException()
         #todo: add cheat
 
         # if name == "anon":
