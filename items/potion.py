0a0cd37043a07625a38460152b0cfe18b88a82cf
Gmyryak Nikita <nikitagmyryak@gmail.com>
2026-05-12 09:23:23 +0200

    expended Potion constructor, now it has 3 levels and 3 types of descr.

---
+++ items/potion.py
@@ -0,0 +1,17 @@
+from items.items import Item
+
+# potion will have 2 or 3 levels, weak/cheap and good/strong potions
+class Potion(Item):
+    def __init__(self, level):
+        self.level = level
+        if level == 1:
+            heal = 25
+            description = "Weak potion"
+        elif level == 2:
+            heal = 50
+            description = "Strong potion"
+        else:
+            health_restore()
+            heal = 6769 #doesnt matter, will restore entire health bar dependent on player level
+            description = "Will heal you entire body"
+        super().__init__(f"Healing Potion [{description}]", heal, 0)
