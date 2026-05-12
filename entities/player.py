c3a326ae8d5cac6e7f46c548176d53e3276e8b0c
Gmyryak Nikita <nikitagmyryak@gmail.com>
2026-05-12 09:49:57 +0200

    added some comments to Player class

--- entities/player.py
+++ entities/player.py
@@ -1,16 +1,40 @@
 from entities.entities import Entity
+import random
 
 class Player(Entity):
-    def __init__(self, name, player_class):
-        super().__init__(name, "Human", 1, 100, 15)
-        self.player_class = player_class
+    # we insert level in the beginning of the game so i can add a cheat later to test staff
+    def __init__(self, name, level=1):
+        max_hp = 100 + (level * 20)
+        base_damage = 15 + (level * 10) # + weapon_damage
+
+        #todo: add cheat
+
+        # if name == "anon":
+        #     cheat()
+
+        super().__init__(name, "Player", level, max_hp, base_damage)
+        self.max_hp = max_hp
+        self.gold = random.randint(25, 101)
         self.inventory = []
+        self.equipped_weapon = None
+        self.capacity = 8 + (level * 2)
         self.description = "Despite everything, it's still you."
 
-    def level_up(self, value):
-        if value > self._level:
-            diff = value - self._level
-            self._level = value
-            self._hp += diff * 20
-            self._damage += diff * 10
-            print(f"Level Up! Now you are level {value}")
+    # todo: redo thi bs
+    # def level_up(self, value):
+    #     if value > self._level:
+    #         self._level = value
+    #         print(f"Level Up! Now you are level {value}")
+
+# todo:
+# [] health_restore()
+# [] heal()
+# [] equip()
+# [] inspect()
+# [] gold/money
+# [] drop_item()
+# [] capacity based on level
+# [] base_damage vs total_damage
+# [] take_damage
+# [] weapon_damage
+# [] cheat
