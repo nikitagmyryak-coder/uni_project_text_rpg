5533d6fa18d9b5095c413625a8efd2afedc186b5
Gmyryak Nikita <nikitagmyryak@gmail.com>
2026-05-14 19:08:04 +0200

    changed alive.py so stats would be more grounded

--- entities/alive.py
+++ entities/alive.py
@@ -2,36 +2,30 @@ from entities.entities import Entity
 
 class Cultist(Entity):
     def __init__(self, level):
-        hp = level * 25
-        damage = level * 25
+        hp = 30 + (level * 10)
+        damage = 5 + (level * 5)
         super().__init__(None, "cultist", level, hp, damage)
-
 
 class ForeignSoldier(Entity):
     def __init__(self, level):
-        hp = 85 + (level * 40)
-        damage = 90 + (level * 45)
+        hp = 50 + (level * 15)
+        damage = 15 + (level * 8)
         super().__init__(None, "foreign_soldier", level, hp, damage)
-
 
 class Mercenary(Entity):
     def __init__(self, level):
-        hp = 55 + (level * 30)
-        damage = 60 + (level * 35)
+        hp = 40 + (level * 12)
+        damage = 10 + (level * 7)
         super().__init__(None, "mercenary", level, hp, damage)
-
 
 class Thug(Entity):
     def __init__(self, level):
-        hp = 40 + (level * 10)
-        damage = 5 + (level * 2)
+        hp = 35 + (level * 8)
+        damage = 5 + (level * 4)
         super().__init__(None, "thug", level, hp, damage)
-
 
 class Ogre(Entity):
     def __init__(self, level):
-        hp = level * 40
-        damage = level * 10
+        hp = 60 + (level * 20)
+        damage = 8 + (level * 6)
         super().__init__(None, "ogre", level, hp, damage)
-
-
