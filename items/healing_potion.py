19156a34b3d5a4623e160c1247067dbca7f75dd6
Gmyryak Nikita <nikitagmyryak@gmail.com>
2026-05-17 11:55:51 +0200

    need do redo health_restore()

--- items/healing_potion.py
+++ items/healing_potion.py
@@ -1,4 +1,5 @@
 from items.items import Item
+from entities.player import *
 
 # potion will have 2 or 3 levels, weak/cheap and good/strong potions
 class HealingPotion(Item):
@@ -10,8 +11,8 @@ class HealingPotion(Item):
         elif level == 2:
             heal = 50
             description = "Strong potion"
-        else:
-            health_restore()
-            heal = 6769 #doesnt matter, will restore entire health bar dependent on player level
-            description = "Will heal you entire body"
+        # else:
+        #     self.health_restore()
+        #     heal = 6769 #doesnt matter, will restore entire health bar dependent on player level
+        #     description = "Will heal you entire body"
         super().__init__(f"Healing Potion [{description}]", heal, 0)
