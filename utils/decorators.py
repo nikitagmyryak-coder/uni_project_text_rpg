fb859c6f35940b2e6387b6bf748eea934a1abbe2
Gmyryak Nikita <nikitagmyryak@gmail.com>
2026-05-18 13:51:48 +0200

    started doing decorator but it is ass

---
+++ utils/decorators.py
@@ -0,0 +1,6 @@
+from entities import player
+
+# def log_action(func):
+#     def wrapper(*args, **kwargs):
+#         if func == player.level_up():
+#             print(f"{player.__name__} has leveled up to LVL: {player._level}")
