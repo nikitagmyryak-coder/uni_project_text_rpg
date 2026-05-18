af01e0df9cc8ca5d1944f4dfd1b55add3aecbd03
Gmyryak Nikita <nikitagmyryak@gmail.com>
2026-05-18 18:23:08 +0200

    made some decorators

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,6 +1,30 @@
 from entities import player
 
-# def log_action(func):
-#     def wrapper(*args, **kwargs):
-#         if func == player.level_up():
-#             print(f"{player.__name__} has leveled up to LVL: {player._level}")
+def log_action(func):
+    def wrapper(*args, **kwargs):
+        # args[0] — это объект Player, так как декоратор применяется к его методам
+        player = args[0]
+
+        action_name = func.__name__.replace('_', ' ').upper()
+
+        print("\n" + "=" * 45)
+        print(f" [ACTION] {player.name} -> {action_name}")
+        print("=" * 45)
+
+        result = func(*args, **kwargs)
+
+        print("." * 45 + "\n")
+        return result
+
+    return wrapper
+
+
+def stat_change_alert(func):
+
+
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print("🌟 STATUS UPDATE SUCCESSFUL 🌟")
+        return result
+
+    return wrapper
